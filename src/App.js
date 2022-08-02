3f658b0d4ab5b30e112cdce70d432d6b1f1cb0fa
caglapekoglu <79510298+caglapekoglu@users.noreply.github.com>
2022-08-02 11:09:24 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import './index.css';
+import SearchMovie from './searchMovie';
+
+function App() {
+  return (
+   <div>
+    <SearchMovie></SearchMovie>
+   </div>
+  );
+}
+
+export default App;
